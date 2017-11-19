bc6e88f6c10d951e16e57553bd036746084069ac
Allan77230 <predaboss@gmail.com>
2017-11-19 17:25:01 +0100

    splash + calligraphy

---
+++ app/src/main/java/fr/alfun/smines/hearthstonecard/activity/SplashScreen.java
@@ -0,0 +1,43 @@
+package fr.alfun.smines.hearthstonecard.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import fr.alfun.smines.hearthstonecard.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+
+/**
+ * Created by aordogh on 17/11/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, AccueilActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+    protected void attachedBaseContext(Context context){
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(context));
+    }
+}
